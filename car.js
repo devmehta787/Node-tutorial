a9fefd38239729ea260b2377a3ca2917994760f1
devmehta <devmehta787@gmail.com>
2023-12-25 23:40:37 +0530

    npm, export functions

---
+++ car.js
@@ -0,0 +1,24 @@
+// const ford = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+// }
+
+// const tesla = {
+//     brand: 'Tesla',
+//     model: 'Model S',
+// }
+
+// // module.exports = car;
+// exports.data = { ford, tesla };
+
+
+
+exports.ford = {
+    brand: 'Ford',
+    model: 'Mustang',
+}
+
+exports.tesla = {
+    brand: 'Tesla',
+    model: 'Model S',
+}
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // getting started with node js
-console.log('This is Node Js Tutorial');
-console.log('My name is Dev Mehta');
-console.log('Jabba Jabba');
+// console.log('This is Node Js Tutorial');
+// console.log('My name is Dev Mehta');
+// console.log('Jabba Jabba');
 // Process.exit(0);
 // process.exitCode = 1;
+
+
+
+const {tesla, ford} = require('./car');
+console.log(JSON.stringify(tesla, null, 2));
+console.log(JSON.stringify(ford, undefined, 5));
