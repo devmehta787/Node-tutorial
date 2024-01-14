a6c68e8c1f5e9f8142448cba607373645b85db3b
devmehta <devmehta787@gmail.com>
2024-01-14 22:53:47 +0530

    structuring express code, restructuring vidly

--- vidly/index.js
+++ vidly/index.js
@@ -1,73 +1,14 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
-app.use(express.json());
-
 const port = process.env.PORT || 3000;
+
+const gener = require('./routes/gener')
+
+app.use(express.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use('/api/geners', gener);
 
-const geners = [
-    { id: 1, name: `Action` },
-    { id: 2, name: `Horror` },
-    { id: 3, name: `Romance` }
-];
-
-//validate the gener
-function validateGener(gener) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(gener, schema);
-}
-
-// get all geners
-app.get('/api/geners', (req, res) => {
-    res.send(geners);
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-
-
-// get a gener with id
-app.get('/api/geners/:id', (req, res) => {
-    const gener = geners.find(g => g.id === parseInt(req.params.id));
-    if (!gener) return res.status(404).send(`The genre with the given ID was not found`);
-    res.send(gener);
-});
-
-// post a gener
-app.post('/api/geners', (req, res) => { 
-    const { error } = validateGener(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    const gener = {
-        id: geners.length + 1,
-        name: req.body.name
-    };
-    geners.push(gener);
-    res.send(geners);
-});
-
-
-// put a gener
-app.put('/api/geners/:id', (req, res) => {
-    const gener = geners.find(g => g.id === parseInt(req.params.id));
-    if (!gener) return res.status(404).send(`The genre with the given ID was not found`);
-
-    const { error } = validateGener(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    gener.name = req.body.name;
-    res.send(geners);
-});
-
-
-// delete a gener
-app.delete('/api/geners/:id', (req, res) => { 
-    const gener = geners.find(g => g.id === parseInt(req.params.id));
-    if (!gener) return res.status(404).send(`The genre with the given ID was not found`);
-
-    const index = geners.indexOf(gener);
-    geners.splice(index, 1);
-
-    res.send(geners);
-});
-
