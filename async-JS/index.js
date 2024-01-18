17a280c7d6ccad2c4b5d203eb74c28613cf3e8c5
devmehta <devmehta787@gmail.com>
2024-01-18 22:40:03 +0530

    patterns for dealing with async code

--- async-JS/index.js
+++ async-JS/index.js
@@ -1,5 +1,27 @@
-console.log('start');
-setTimeout(() => {
-    console.log('Reading user from database...');
-}, 3000);
-console.log('start 2');
+// sync async code
+
+// console.log('start');
+// setTimeout(() => {
+//     console.log('Reading user from database...');
+// }, 3000);
+// console.log('start 2');
+
+
+// patterns for dealing with async code
+
+// callbacks
+// promises
+// async/await
+
+console.log('Before');
+const user = getUser(1);
+console.log(user);
+console.log('After');
+
+function getUser(id) { 
+    setTimeout(() => {
+        console.log('Reading user from database...');
+        return { id: id, gitHubUsername: 'mosh' };
+    }, 2000);
+    // return 1;
+}
