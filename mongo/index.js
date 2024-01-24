072d667d7ac4078465f775af9ef233d93c54f66a
devmehta <devmehta787@gmail.com>
2024-01-24 22:36:38 +0530

    Comparison Query Operators added

--- mongo/index.js
+++ mongo/index.js
@@ -25,12 +25,39 @@ async function createCourse() {
     const result = await course.save();
     console.log(result);
 }
+// createCourse();
 
+// async function getCourses() {
+//     const courses = await Course
+//         .find({ author: 'Mosh', isPublished: true, })
+//         .limit(10)
+//         .sort({ name: 1 })
+//         .select({ name: 1, tags: 1 });
+//     console.log(courses);
+// }
+
+// compaison query operators
+
+// eq (equal)
+// ne (not equal)
+// gt (greater than)
+// gte (greater than or equal to)
+// lt (less than)
+// lte (less than or equal to)
+// in
+// nin (not in)
+
+// logical query operators
 async function getCourses() {
-    const courses = await Course.find({
-        author: 'Mosh',
-        isPublished: true,
-    });
+    const courses = await Course
+        // gives courses with price between 10 and 20
+        .find({ price: { $gte: 10, $lte: 20 } })
+
+        // gives courses with price equal to 10, 15, 20
+        // .find({ price: { $in: [10, 15, 20] } })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
     console.log(courses);
 }
 
