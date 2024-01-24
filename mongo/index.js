711f63e0923af26e59f84a3285f0853e05fbde5b
devmehta <devmehta787@gmail.com>
2024-01-24 23:40:22 +0530

    exercises- getCourses, with sorting the data, with given words

--- mongo/index.js
+++ mongo/index.js
@@ -47,18 +47,80 @@ async function createCourse() {
 // in
 // nin (not in)
 
-// logical query operators
-async function getCourses() {
-    const courses = await Course
-        // gives courses with price between 10 and 20
-        .find({ price: { $gte: 10, $lte: 20 } })
+// async function getCourses() {
+//     const courses = await Course
+//         // gives courses with price between 10 and 20
+//         .find({ price: { $gte: 10, $lte: 20 } })
 
-        // gives courses with price equal to 10, 15, 20
-        // .find({ price: { $in: [10, 15, 20] } })
-        .limit(10)
-        .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    console.log(courses);
-}
+//         // gives courses with price equal to 10, 15, 20
+//         .find({ price: { $in: [10, 15, 20] } })
+//         .limit(10)
+//         .sort({ name: 1 })
+//         .select({ name: 1, tags: 1 });
+//     console.log(courses);
+// }
+    
+// // logical query operators
+
+// // or
+// // and
+
+// async function getCourses() {
+//     const courses = await Course
+//         // .find()
+//         // .or([{ author: 'Mosh' }, { isPublished: true }])
+//         .find()
+//         .and([{ author: 'Mosh' }, { isPublished: true }])
+//         .limit(10)
+//         .sort({ name: 1 })
+//         .select({ name: 1, tags: 1 });
+//     console.log(courses);
+// }
+
+// // Regular Expressions
+// async function getCourses() {
+//     const courses = await Course
+        
+//     // starts with Mosh
+//         .find({ author: /^Mosh/ })
+        
+//     //ends with Hamedani
+//     // adding an i at the end makes it case insensitive
+//         .find({ author: /Hamedani$/i })
+    
+//     // contains Mosh
+//         .find({ author: /.*Mosh.*/i })
+//         .limit(10)
+//         .sort({ name: 1 })
+//         .select({ name: 1, tags: 1 });
+//     console.log(courses);
+// }
+
+
+// // counting
+
+// async function getCourses() {
+//     const courses = await Course
+//         .find({ author: /.*Mosh.*/i })
+//         .limit(10)
+//         .sort({ name: 1 })
+//         .count();
+//     console.log(courses);
+// }
+
+// // pagination
+// // /api/courses?pageNumber=2&pageSize=10
+
+// async function getCourses() {
+//     const pageNumber = 2;
+//     const pageSize = 10;
+//     const courses = await Course
+//         .find({ author: /.*Mosh.*/i })
+//         .skip((pageNumber - 1) * pageSize)
+//         .limit(pageSize)
+//         .sort({ name: 1 })
+//         .count();
+//     console.log(courses);
+// }
 
 getCourses();
