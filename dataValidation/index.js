cc51f182036435e983ec4f4d7990e66bb4ca4f94
devmehta <devmehta787@gmail.com>
2024-01-26 22:42:23 +0530

    custom validator for a property which does not have a default validator

--- dataValidation/index.js
+++ dataValidation/index.js
@@ -6,26 +6,52 @@ mongoose.connect('mongodb://localhost:27017/test')
 
 const courseSchema = new mongoose.Schema({
     // name: String,
-    name: { type: String, required: true },
+    // data type, required, validation
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'],
+    },
     author: String,
-    tags: [String],
+    // tags: [String],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag.',
+        },  
+    },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function () { return this.isPublished; }
+    },
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        // name: 'React Course',
+        name: 'Course Abcd',
+        category: 'web',
         author: 'Dev',
-        tags: ['node', 'backend'],
+        tags: null,
         isPublished: true,
+        price:10
     });
     try {
-        await course.validate();
-        // const result = await course.save();
-        // console.log(result);
+        // await course.validate();
+        const result = await course.save();
+        console.log(result);
     }
     catch (ex) {
         console.log(ex.message);
