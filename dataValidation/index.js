bdcb7d7abbf14e1feb2ca40d7c8054165d753acf
devmehta <devmehta787@gmail.com>
2024-01-29 23:40:46 +0530

    async validator is no more used instead use promises

--- dataValidation/index.js
+++ dataValidation/index.js
@@ -20,14 +20,44 @@ const courseSchema = new mongoose.Schema({
     },
     author: String,
     // tags: [String],
+    
+    // custom validation
+    
+    // tags: {
+    //     type: Array,
+    //     validate: {
+    //         validator: function (v) {
+    //             return v && v.length > 0;
+    //         },
+    //         message: 'A course should have at least one tag.',
+    //     },  
+    // },
+    
+    // async validation
+    
     tags: {
         type: Array,
-        validate: {
-            validator: function (v) {
-                return v && v.length > 0;
-            },
-            message: 'A course should have at least one tag.',
-        },  
+        // validate: {
+        //     validator: function (v) {
+        //         setTimeout(() => {
+        //             return new Promise((resolve, reject) => {
+        //                 const result = v && v.length > 0;
+        //                 return result;
+        //             }, 4000);
+        //         })
+        //     },
+        //     message: 'A course should have at least one tag.',
+        // }
+        validate: function(v) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const result = v && v.length > 0;
+                    if (!result) {
+                        reject(new Error());
+                    }
+                }, 4000);
+            });
+        },
     },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
@@ -44,12 +74,12 @@ async function createCourse() {
         name: 'Course Abcd',
         category: 'web',
         author: 'Dev',
-        tags: null,
+        tags: [],
         isPublished: true,
         price:10
     });
     try {
-        // await course.validate();
+        await course.validate();
         const result = await course.save();
         console.log(result);
     }
