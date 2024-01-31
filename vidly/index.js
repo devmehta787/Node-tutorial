dd3ac30207195a8a4263d0008019502eb3f9afb7
devmehta <devmehta787@gmail.com>
2024-01-31 23:41:55 +0530

    vidly connected to mongo, added async await functions

--- vidly/index.js
+++ vidly/index.js
@@ -1,9 +1,14 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const gener = require('./routes/gener')
+
+mongoose.connect('mongodb://localhost:27017/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(express.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
